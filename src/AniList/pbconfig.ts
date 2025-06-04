4a7382762b08ca2f4c2ba6a75028352cec284d4f
Eduard Smet <contact@celarye.dev>
2025-06-04 16:36:43 +0200

    bug(AniList): Update the getMangaProgress returned data according to the recent app changes

--- src/AniList/pbconfig.ts
+++ src/AniList/pbconfig.ts
@@ -4,7 +4,7 @@ export default {
     name: "AniList",
     description:
         "Extension that integrates with anilist.co for tracking and collection management.",
-    version: "1.0.0-alpha.5",
+    version: "1.0.0-alpha.6",
     icon: "icon.png",
     language: "en",
     contentRating: ContentRating.EVERYONE,
