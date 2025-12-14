f9c0d809e08724be02889144b584002940179f18
Eduard Smet <contact@celarye.dev>
2025-12-15 00:46:07 +0100

    chore: Update dependencies

--- src/MangaUpdates/pbconfig.ts
+++ src/MangaUpdates/pbconfig.ts
@@ -1,19 +1,19 @@
-import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
+import { ContentRating, ExtensionInfo, SourceIntents } from "@paperback/types";
 
 export default {
   name: "MangaUpdates",
   description:
     "Extension that integrates with mangaupdates.com for tracking and collection management.",
-  version: "1.0.0-alpha.2",
+  version: "1.0.0-alpha.3",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
   capabilities:
-    SourceIntents.SETTINGS_UI |
-    SourceIntents.DISCOVER_SECIONS |
-    SourceIntents.MANGA_SEARCH |
-    SourceIntents.MANGA_PROGRESS,
-  // TODO: SourceIntents.COLLECTION_MANAGEMENT - not yet implemented,
+    SourceIntents.SETTINGS_FORM_PROVIDING |
+    SourceIntents.DISCOVER_SECIONS_PROVIDING |
+    SourceIntents.SEARCH_RESULTS_PROVIDING |
+    SourceIntents.MANGA_PROGRESS_PROVIDING,
+  // TODO: SourceIntents.COLLECTION_MANAGEMENT
   badges: [],
   developers: [
     {
@@ -21,4 +21,4 @@ export default {
       github: "https://github.com/IntermittentlyRupert",
     },
   ],
-} as SourceInfo;
+} as ExtensionInfo;
