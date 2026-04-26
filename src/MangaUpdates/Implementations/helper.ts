6ec5a628aaf66ed822f98762ae6cc081a711d7ef
Nicholas Lim <6513413+niclimcy@users.noreply.github.com>
2026-04-26 14:00:07 +0800

    chore: Remove unused eslint-disable comments

--- src/MangaUpdates/Implementations/helper.ts
+++ src/MangaUpdates/Implementations/helper.ts
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /* Copyright © 2026 Inkdex */
 
-/* eslint-disable */
 export function applyMixins(derivedCtor: any, constructors: any[]) {
   constructors.forEach((baseCtor) => {
     Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
